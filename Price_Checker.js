e77d596d67433b97bf7348fae7a019d1064365bd
Taylor Caton <taylorcaton@gmail.com>
2018-07-04 18:25:24 -0400

    Starting to convert to a class structure

---
+++ Price_Checker.js
@@ -0,0 +1,18 @@
+const checker = require('./checker.js');
+
+module.exports = class Price_Checker {
+
+  constructor(gameList){
+    this.gameList = gameList;
+    this.pricedList = [];
+    this.valueTotal = 0;
+  }
+
+  async buildPriceList(){
+
+  }
+
+  async getPriceOfSingleGame(){
+
+  }
+};
